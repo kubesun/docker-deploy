09e6dffc655b472b7ada87e534480a0abaf42fdc
mandala <rcc@vip.qq.com>
2024-02-14 13:43:39 +0800

    perf: :zap: 完善安装流程

--- gitlab/gitlab.sh
+++ gitlab/gitlab.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 set -x
 
-mkdir -p /home/gitlab/config
-mkdir -p /home/gitlab/logs
-mkdir -p /home/gitlab/data
+config="/mnt/data/docker-gitlab/config"
+logs="/mnt/data/docker-gitlab/logs"
+data="/mnt/data/docker-gitlab/data"
+mkdir -p $config
+mkdir -p $logs
+mkdir -p $data
 
+host="192.168.2.100"
+http_port="7080"
+https_port="7443"
+ssh_port="2222"
 docker run -d \
--h 192.168.2.158 \
--p 6080:80 \
--p 6443:443 \
--p 2222:22 \
+-h $host \
+-p $http_port:80 \
+-p $https_port:443 \
+-p $ssh_port:22 \
 -e TZ=Asia/Shanghai \
 --shm-size 256m \
 --name gitlab \
 --restart always \
--v /home/gitlab/config:/etc/gitlab \
--v /home/gitlab/logs:/var/log/gitlab \
--v /home/gitlab/data:/var/opt/gitlab \
+-v $config:/etc/gitlab \
+-v $logs:/var/log/gitlab \
+-v $data:/var/opt/gitlab \
 gitlab/gitlab-ce:latest
 
-vi /home/gitlab/config/gitlab.rb
+# 查看密码, 账号默认是root
+cat $config/initial_root_password
+
+
+# vi $config/gitlab.rb
 
 set +x
