5f31510c99ef85858d47b91cdaf838740d5eecdf
mandala <rcc@vip.qq.com>
2024-03-27 03:35:26 +0800

    feat: add nginx common

---
+++ nginx/quic/README.md
@@ -0,0 +1,54 @@
+## 说明
+
+1. 本库意为快速搭建 `NGINX`的`QUIC` + `HTTP3`, 配置方便
+2. 脚本安全, 不会保存你的任何数据
+3. 开源, 任何人都可以查看代码
+
+## 原理
+
+1. 使用别人通过Linux发行版的打包用于QUIC编译的NGINX二进制文件和它用于QUIC+HTTP3的配置, 然后发行到Docker之类的容器注册表
+2. 利用别人封装好的容器, 然后再注入自己的配置
+
+## 快速入门
+
+> !确保/home/nginx目录没有重要文件, 默认使用该目录
+
+1. 定义你的域名`DOMAIN`, 例如`example.com`, 不要加前缀!, 脚本已自动加
+   示例:
+   ```shell
+   export DOMAIN="example.com"
+   ```
+
+2. 定义`WEB_DIR`目录, 默认是`/home/web`, 该目录是前端静态文件, 只能是原生的js, html, css, 是框架(如果有)编译后的文件
+   示例:
+   ```shell
+   export WEB_DIR="/home/web"
+   ```
+
+3. 上传ssl文件, 把`nginx.crt`PEM证书与`nginx.key`证书密钥文件到`/home/nginx/ssl`目录下, 确保只有一个文件后缀,
+   不能同时存在两个`.crt`或者`.key`文件, 会导致脚本出问题
+
+4. 执行脚本
+
+```shell
+sudo chmod +x ./config.sh
+sudo ./config.sh
+```
+
+## 自定义
+
+当你熟悉之后, 可以自定义了, 这是变量列表:
+
+- WEB_DIR: web目录
+- NGINX_DIR: NGINX的目录, 推荐用于保存配置文件与SSL文件
+- CONF_DIR: NGINX的配置文件目录, 一般定义在NGINX的目录的目录下
+- SSL_DIR: NGINX的SSL文件目录, 一般定义在NGINX的目录的目录下
+
+默认值:
+
+```shell
+WEB_DIR="/home/web"
+NGINX_DIR="/home/nginx"
+CONF_DIR="/home/nginx/conf"
+SSL_DIR="/home/nginx/ssl"
+```
---
+++ nginx/quic/config.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+if [ -z "${DOMAIN}" ]; then
+    exit 1
+    echo "请编写你的域名"
+fi
+
+# 判断 NGINX_DIR 变量是否已定义，如果未定义，则设置默认值
+if [ -z "${NGINX_DIR}" ]; then
+    export NGINX_DIR="/home/nginx"
+fi
+
+# 判断 WEB_DIR 变量是否已定义，如果未定义，则设置默认值
+if [ -z "${WEB_DIR}" ]; then
+    export WEB_DIR="/home/web"
+fi
+
+# 判断 CONF_DIR 变量是否已定义，如果未定义，则设置默认值
+if [ -z "${CONF_DIR}" ]; then
+    export CONF_DIR="/home/nginx/conf"
+fi
+
+# 判断 SSL_DIR 变量是否已定义，如果未定义，则设置默认值
+if [ -z "${SSL_DIR}" ]; then
+    export SSL_DIR="/home/nginx/ssl"
+fi
+
+mkdir -p $WEB_DIR
+mkdir -p $CONF_DIR
+mkdir -p $SSL_DIR
+cd $NGINX_DIR || exit
+
+cp $SSL_DIR/*.crt $SSL_DIR/nginx.crt
+cp $SSL_DIR/*.key $SSL_DIR/nginx.key
+
+# https://github.com/macbre/docker-nginx-http3
+docker pull ghcr.io/macbre/nginx-http3:latest
+
+cat > /home/nginx/conf/nginx.conf <<EOF
+server {
+    listen 80;
+    server_name server_name; # server_name
+    return 301 https://host$request_uri; # webside
+}
+
+server {
+    server_name ${DOMAIN} www.${DOMAIN};  # 服务器名称
+
+    # UDP listener for QUIC+HTTP/3
+    # http/3
+    listen 443 quic reuseport;
+
+    # http/2 and http/1.1
+    listen 443 ssl;
+    http2 on;
+
+    # 以下为各种 HTTP 安全相关头部的设置
+    add_header Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
+    add_header X-XSS-Protection "1; mode=block" always;
+    add_header X-Frame-Options SAMEORIGIN always;
+    add_header X-Content-Type-Options nosniff;
+    add_header X-Frame-Options "DENY";
+    add_header Alt-Svc 'h3=":443"; ma=86400, h3-29=":443"; ma=86400';
+
+    # SSL/TLS 相关配置
+    ssl_protocols TLSv1.3 TLSv1.2;  # 设置支持的 SSL 协议版本
+    # ssl_ciphers ...;  # 设置 SSL 密码套件
+    ssl_prefer_server_ciphers on;  # 优先使用服务器的密码套件
+    ssl_ecdh_curve X25519:P-256:P-384;  # 设置 ECDH 曲线
+    ssl_early_data on;  # 启用 TLS 1.3 的 0-RTT 特性
+    ssl_stapling on;  # 启用 OCSP Stapling
+    ssl_stapling_verify on;  # 启用 OCSP Stapling 的验证
+
+    # SSL 证书路径配置
+    ssl_certificate     /etc/nginx/ssl/nginx.crt;  # SSL 证书路径
+    ssl_certificate_key /etc/nginx/ssl/nginx.key;  # SSL 证书密钥路径
+
+    location / {
+        root   /etc/nginx/html;  # 设置根目录路径
+        index  index.html index.htm default.html default.htm;  # 设置默认index首页文件
+    }
+}
+
+EOF
+
+docker stop nginx-quic || true
+docker rm nginx-quic || true
+
+docker run -itd \
+--name nginx-quic \
+-v ${WEB_DIR}:/etc/nginx/html \
+-v ${CONF_DIR}:/etc/nginx/conf.d \
+-v ${SSL_DIR}:/etc/nginx/ssl \
+-p '443:443/tcp' \
+-p '443:443/udp' \
+-p 80:80 \
+ghcr.io/macbre/nginx-http3
+
+echo "正在查看日志, 按 Ctrl + C 退出"
+docker logs -f nginx-quic
